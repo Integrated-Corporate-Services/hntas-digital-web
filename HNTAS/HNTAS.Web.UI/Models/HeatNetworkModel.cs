c52c4eaecd616121d3c425bdd73edd8429429c9d
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2025-07-21 12:01:36 +0100

    HN name model added

---
+++ HNTAS/HNTAS.Web.UI/Models/HeatNetworkModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HNTAS.Web.UI.Models
+{
+    public class HeatNetworkNameModel
+    {
+        [Required(ErrorMessage = "Please enter the heat network name.")]
+        [StringLength(100, ErrorMessage = "The heat network name cannot exceed 100 characters.")]
+        public string HeatNetworkName { get; set; }
+    }
+    
+}
