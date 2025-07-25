aea850a4e47821daea48f7a245ab072ee3ef8fb5
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2025-07-25 14:46:45 +0100

    user account page wip

---
+++ HNTAS/HNTAS.Web.UI/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserAccount()
+        {
+
+            return View();
+        }
+    }
+}
