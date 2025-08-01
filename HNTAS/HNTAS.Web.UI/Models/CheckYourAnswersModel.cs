48b14d932ff16b5846a0f43861d112392b59d930
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-08-01 13:59:57 +0100

    Moved contact detail to organisation changes check you answer page logic Updated links in check you answer page Moved sign out button to the header

--- HNTAS/HNTAS.Web.UI/Models/CheckYourAnswersModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/CheckYourAnswersModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HNTAS.Web.UI.Models
 {
-    public class CheckAnswersModel
+    public class CheckYourAnswersModel
     {
         // Use [BindNever] to prevent the model binder from attempting to bind these
         // properties from the incoming form data on a POST request.
