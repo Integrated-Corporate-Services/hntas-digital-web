22607ac546bcae1ab823dabe309dbb2059299d83
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-08-11 10:59:07 +0100

    Accessibility Issues and added back button and added required all the fields in the address

--- HNTAS/HNTAS.Web.UI/Models/CompaniesHouse/RegisteredOfficeAddressModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/CompaniesHouse/RegisteredOfficeAddressModel.cs
@@ -7,9 +7,14 @@ namespace HNTAS.Web.UI.Models.CompaniesHouse
         [Required(ErrorMessage = "Street address is required.")]
         public string? AddressLine1 { get; set; }
         public string? AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Town or city is required.")]
         public string? Locality { get; set; } // City/Town
+
         [Required(ErrorMessage = "Postal code or zip code is required.")]
         public string? PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
         public string? Country { get; set; }
     }
 }
