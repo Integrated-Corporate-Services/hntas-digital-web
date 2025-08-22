63e0741398d4877a483fbce87c9438bbfcd74d51
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2025-08-22 12:38:33 +0100

    Feature/dhb 186 dh accepts invite (#107)
    * added duty holder invitation, start and landing page
    
    * added one log in after StartPage before user accesses the Dashboard
    
    * renamed dutyholder to contributor

--- HNTAS/HNTAS.Web.UI/Controllers/HeatNetworkEligibilityController.cs
+++ HNTAS/HNTAS.Web.UI/Controllers/HeatNetworkEligibilityController.cs
@@ -87,7 +87,7 @@ namespace HNTAS.Web.UI.Controllers
                     _sessionHelper.SaveToSession<WhereIsTheHeatNetworkViewModel>(HttpContext, SessionKeys.WhereIsTheHeatNetworkModelKey, model);
                     return RedirectToAction("LocatedInNorthernIreland", "HeatNetworkEligibility");
                 default:
-                    ModelState.AddModelError(string.Empty, "Please select a valid option.");
+                    ModelState.AddModelError(nameof(model.PartOfTheUK), "Please select a valid option.");
                     return View(model);
             }
 
@@ -119,7 +119,7 @@ namespace HNTAS.Web.UI.Controllers
                     _sessionHelper.SaveToSession<HowManyDwellingsIncludedViewModel>(HttpContext, SessionKeys.HowManyDwellingsIncludedModelKey, model);
                     return RedirectToAction("FewerThan10Dwellings", "HeatNetworkEligibility");
                 default:
-                    ModelState.AddModelError(string.Empty, "Please select a valid option.");
+                    ModelState.AddModelError(nameof(model.NumberOfDwellings), "Please select a valid option.");
                     return View(model);
             }
         }
@@ -150,7 +150,7 @@ namespace HNTAS.Web.UI.Controllers
                     _sessionHelper.SaveToSession<IsHNCurrentlyOperatingViewModel>(HttpContext, SessionKeys.IsHNCurrentlyOperatingModelKey, model);
                     return RedirectToAction("HaveYouSignedMEContract", "HeatNetworkEligibility");
                 default:
-                    ModelState.AddModelError(string.Empty, "Please select a valid option.");
+                    ModelState.AddModelError(nameof(model.IsCurrentlyOperating), "Please select a valid option.");
                     return View(model);
             }
         }
@@ -181,7 +181,7 @@ namespace HNTAS.Web.UI.Controllers
                     _sessionHelper.SaveToSession<HaveYouSignedMEContractViewModel>(HttpContext, SessionKeys.HaveYouSignedMEContractModelKey, model);
                     return RedirectToAction("YouAreEligible", "HeatNetworkEligibility");
                 default:
-                    ModelState.AddModelError(string.Empty, "Please select a valid option.");
+                    ModelState.AddModelError(nameof(model.HaveYouSignedMEContract), "Please select a valid option.");
                     return View(model);
             }
         }
--- HNTAS/HNTAS.Web.UI/Helpers/SessionKeys.cs
+++ HNTAS/HNTAS.Web.UI/Helpers/SessionKeys.cs
@@ -20,5 +20,7 @@
 
         public const string HeatNetworkLocationModelKey = "HeatNetworkLocation";
         public const string HeatNetworkNameModelKey = "HeatNetworkName";
+
+        public const string YouHaveBeenInvitedModelKey = "YouHaveBeenInvited";
     }
 }
