7744dac7c0e9bcec558cf7f40a87236809b34c48
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2025-10-21 14:59:47 +0100

    added testcases for ContributorController

--- HNTAS/HNTAS.Web.UI.Tests/Contollers/ContributorControllerTests.cs
+++ HNTAS/HNTAS.Web.UI.Tests/Contollers/ContributorControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using HNTAS.Web.UI.Controllers;
+﻿using HNTAS.Api.Client.Model;
+using HNTAS.Web.UI.Controllers;
 using HNTAS.Web.UI.Helpers;
 using HNTAS.Web.UI.Models;
 using HNTAS.Web.UI.Services;
@@ -6,8 +7,11 @@ using HNTAS.Web.UI.Services.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System.Net.Http;
+using System.Security.Claims;
 
 namespace HNTAS.Web.UI.Tests.Controllers
 {
@@ -33,87 +37,223 @@ namespace HNTAS.Web.UI.Tests.Controllers
 
         private ContributorController CreateController()
         {
-            var controller = new ContributorController(_userServiceMock.Object, _invitationServiceMock.Object, _loggerMock.Object, _sessionHelperMock.Object, _invitationTokenService.Object);
+            var controller = new ContributorController(
+                _userServiceMock.Object,
+                _invitationServiceMock.Object,
+                _loggerMock.Object,
+                _sessionHelperMock.Object,
+                _invitationTokenService.Object
+                );
             controller.ControllerContext = new ControllerContext
             {
                 HttpContext = new DefaultHttpContext()
             };
-            var urlHelperMock = new Mock<IUrlHelper>();
-            urlHelperMock
-                .Setup(x => x.Action(It.IsAny<UrlActionContext>()))
-                .Returns("/mocked-url");
-
-            controller.Url = urlHelperMock.Object;
             return controller;
         }
 
+        private Mock<IUrlHelper> SetUpBackLink(string controller, string action)
+        {
+            var urlHelperMock = new Mock<IUrlHelper>();
+            urlHelperMock
+                .Setup(u => u.Action(It.Is<UrlActionContext>(ctx =>
+                    ctx.Action == action && ctx.Controller == controller)))
+                .Returns($"{controller}/{action}");
+            return urlHelperMock;
+        }
 
+        private UserDetailsResponse MockGetUserDetailsResponse(string userId)
+        {
+            var userDetails = new HNTAS.Api.Client.Model.UserDetailsResponse
+            {
+                Id = userId,
+                OneLoginId = "one-login-id",
+                FirstName = "Test",
+                LastName = "User",
+                FullName = "Test User",
+                EmailId = "test@email.com",
+                JobTitle = "Assessor",
+                MobileNumber = "1234567890",
+                Status = "Active",
+                Roles = new List<UserRole> { UserRole.Assessor },
+                Organisation = new OrganisationResponse
+                {
+                    OrgId = "org-id",
+                    Name = "Test Organisation",
+                    CompaniesHouseNumber = "12345678",
+                    Type = OrganisationType.UkCompaniesHouse,
+                    RegisteredAddress = new RegisteredAddress("123 Test St", "TE1 1ST", "Test Area", "Test Town", "Test County", "Test Country")
+                },
+                HeatNetworks = new List<HeatNetworkUserResponse>()
+                {
+                    new HeatNetworkUserResponse
+                    {
+                        HnId = "hn-1",
+                        Name = "Heat Network 1",
+                        Location = "Location 1"
+                    },
+                    new HeatNetworkUserResponse
+                    {
+                        HnId = "hn-2",
+                        Name = "Heat Network 2",
+                        Location = "Location 2"
+                    }
+                }
+            };
+
+            return userDetails;
+        }
+
+        private InvitedUserResponse MockGetInvitationByIdAsync(string invitationId)
+        {
+            var invitedUser = new InvitedUserResponse
+            {
+                Id = invitationId,
+                InviterUserId = "valid-user-id",
+                Email = "",
+                FirstName = "Invited",
+                LastName = "User",
+                FullName = "Invited User",
+                Status = InvitationStatus.Invited,
+                InvitedHnId = "",
+                Roles = new List<ContributorRole> { ContributorRole.Assessor },
+                InvitedAt = DateTimeOffset.UtcNow,
+                AcceptedAt = null,
+                RejectedAt = null
+            };
+            return invitedUser;
+        }
+
+        #region testcases done
 
         [Fact]
-        public async Task Get_YouHaveBeenInvited_WithSessionData_ReturnsViewWithModel()
+        public async Task Get_YouHaveBeenInvited_ValidToken_ReturnsViewWithModel()
         {
-            var expectedModel = new YouHaveBeenInvitedModel { AcceptInvitation = "accept" };
-            _sessionHelperMock
-                .Setup(x => x.GetFromSession<YouHaveBeenInvitedModel>(
-                    It.IsAny<HttpContext>(), SessionKeys.YouHaveBeenInvitedModelKey))
-                .Returns(expectedModel);
-            var result = await _controller.YouHaveBeenInvited() as ViewResult;
+            // Arrange
+            var token = "validToken";
+            var invitationId = "validid";
+            var invitationEmail = "test@mailinator.com";
+            var invitation = MockGetInvitationByIdAsync(invitationId);
+            var inviterUser = MockGetUserDetailsResponse(invitation.InviterUserId);
 
-            Assert.NotNull(result);
-            Assert.IsType<YouHaveBeenInvitedModel>(result.Model);
-            Assert.Equal("accept", ((YouHaveBeenInvitedModel)result.Model).AcceptInvitation);
+            _invitationTokenService.Setup(s => s.DecryptToken(token))
+                .Returns((invitationId, invitationEmail));
+            _invitationServiceMock.Setup(s => s.GetInvitationByIdAsync(invitationId)).ReturnsAsync(invitation);
+            _userServiceMock.Setup(s => s.GetUserDetails(invitation.InviterUserId)).ReturnsAsync(inviterUser);
+            var tempData = new TempDataDictionary(_controller.HttpContext, Mock.Of<ITempDataProvider>());
+            _controller.TempData = tempData;
+
+
+
+            _controller.ControllerContext.HttpContext.Request.QueryString = new QueryString($"?token={token}");
+
+            // Act
+            var result = await _controller.YouHaveBeenInvited();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult.Model);
+            _sessionHelperMock.Verify(s => s.SaveToSession(It.IsAny<HttpContext>(), SessionKeys.InvitedTokenEmail, invitationEmail), Times.Once);
         }
 
         [Fact]
-        public async Task Get_YouHaveBeenInvited_WithoutSessionData_ReturnsViewWithNewModel()
+        public async Task Get_YouHaveBeenInvited_MissingToken_SetsErrorMessageAndReturnsView()
         {
-            _sessionHelperMock
-                .Setup(x => x.GetFromSession<YouHaveBeenInvitedModel>(
-                    It.IsAny<HttpContext>(), SessionKeys.YouHaveBeenInvitedModelKey))
-                .Returns((YouHaveBeenInvitedModel)null);
-            var result = await _controller.YouHaveBeenInvited() as ViewResult;
+            // Arrange
+            // Token is missing, so no setup needed for DecryptToken or service calls as they won't be invoked
+            var tempData = new TempDataDictionary(_controller.HttpContext, Mock.Of<ITempDataProvider>());
+            _controller.TempData = tempData;
 
-            Assert.NotNull(result);
-            Assert.IsType<YouHaveBeenInvitedModel>(result.Model);
+            // Act
+            var result = await _controller.YouHaveBeenInvited();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.True(_controller.TempData.ContainsKey("ErrorMessage"));
+            Assert.Equal("The invitation token is missing from your request. Please use the link provided in the invitation email to proceed.", _controller.TempData["ErrorMessage"]);
         }
 
         [Fact]
-        public async Task Post_YouHaveBeenInvited_InvalidModel_ReturnsView()
+        public async Task Post_YouHaveBeenInvitedAsync_ValidModel_AcceptInvitation_RedirectsToStartPage()
         {
+            // Arrange
+            var model = new YouHaveBeenInvitedModel { AcceptInvitation = "accept" };
+            var invitationId = "validid";
+            _sessionHelperMock.Setup(s => s.GetFromSession<string>(It.IsAny<HttpContext>(), SessionKeys.InvitationId)).Returns(invitationId);
+
+            // Act
+            var result = await _controller.YouHaveBeenInvitedAsync(model);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("StartPage", redirectResult.ActionName);
+            Assert.Equal("Home", redirectResult.ControllerName);
+        }
+
+        [Fact]
+        public async Task Post_YouHaveBeenInvitedAsync_InvalidModelState_ReturnsViewWithModel()
+        {
+            // Arrange
+            var model = new YouHaveBeenInvitedModel { AcceptInvitation = "accept" };
+            var tempData = new TempDataDictionary(_controller.HttpContext, Mock.Of<ITempDataProvider>());
+            _controller.TempData = tempData;
             _controller.ModelState.AddModelError("AcceptInvitation", "Required");
 
-            var result = await _controller.YouHaveBeenInvitedAsync(new YouHaveBeenInvitedModel()) as ViewResult;
+            // Act
+            var result = await _controller.YouHaveBeenInvitedAsync(model);
 
-            Assert.NotNull(result);
-            Assert.IsType<ViewResult>(result);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal(model, viewResult.Model);
         }
 
         [Fact]
-        public async Task Post_YouHaveBeenInvited_Accept_RedirectsToStartPageAsync()
+        public async Task Post_YouHaveBeenInvitedAsync_DeclineInvitation_RedirectsToYouHaveDeclined()
         {
-            var model = new YouHaveBeenInvitedModel { AcceptInvitation = "accept" };
-            var result = (await _controller.YouHaveBeenInvitedAsync(model)) as RedirectToActionResult;
-
-            Assert.NotNull(result);
-            Assert.Equal("StartPage", result.ActionName);
-            Assert.Equal("Contributor", result.ControllerName);
-        }
-
-        [Fact]
-        public async Task Post_YouHaveBeenInvited_Decline_RedirectsToYouHaveDeclinedAsync()
-        {
+            // Arrange
             var model = new YouHaveBeenInvitedModel { AcceptInvitation = "decline" };
-            var result = await _controller.YouHaveBeenInvitedAsync(model) as RedirectToActionResult;
+            var invitationId = "validid";
+            _sessionHelperMock.Setup(s => s.GetFromSession<string>(It.IsAny<HttpContext>(), SessionKeys.InvitationId)).Returns(invitationId);
+            _invitationServiceMock.Setup(s => s.RejectInvitationAsync(invitationId)).Returns(Task.CompletedTask);
 
-            Assert.NotNull(result);
-            Assert.Equal("YouHaveDeclined", result.ActionName);
-            Assert.Equal("Contributor", result.ControllerName);
+
+            // Act
+            var result = await _controller.YouHaveBeenInvitedAsync(model);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("YouHaveDeclined", redirectResult.ActionName);
+            Assert.Equal("Contributor", redirectResult.ControllerName);
+            _invitationServiceMock.Verify(s => s.RejectInvitationAsync(invitationId), Times.Once);
+        }
+
+        [Fact]
+        public async Task YouHaveBeenInvitedAsync_DeclineInvitation_Exception_ReturnsViewWithErrorMessage()
+        {
+            // Arrange
+            var model = new YouHaveBeenInvitedModel { AcceptInvitation = "decline" };
+            var invitationId = Guid.NewGuid().ToString();
+            _invitationServiceMock.Setup(s => s.RejectInvitationAsync(invitationId)).ThrowsAsync(new Exception("Database error"));
+            _sessionHelperMock.Setup(s => s.GetFromSession<string>(It.IsAny<HttpContext>(), SessionKeys.InvitationId)).Returns(invitationId);
+            var tempData = new TempDataDictionary(_controller.HttpContext, Mock.Of<ITempDataProvider>());
+            _controller.TempData = tempData;
+
+            // Act
+            var result = await _controller.YouHaveBeenInvitedAsync(model);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal(model, viewResult.Model);
+            Assert.True(_controller.TempData.ContainsKey("ErrorMessage"));
+            Assert.Equal("An error occurred while declining the invitation. Please try again later.", _controller.TempData["ErrorMessage"]);
         }
 
         [Fact]
         public async Task Post_YouHaveBeenInvited_InvalidChoice_ReturnsViewWithError()
         {
             var model = new YouHaveBeenInvitedModel { AcceptInvitation = "maybe" };
+            var invitationId = "validid";
+            _sessionHelperMock.Setup(s => s.GetFromSession<string>(It.IsAny<HttpContext>(), SessionKeys.InvitationId)).Returns(invitationId);
+            _invitationServiceMock.Setup(s => s.RejectInvitationAsync(invitationId)).Returns(Task.CompletedTask);
             var result = await _controller.YouHaveBeenInvitedAsync(model) as ViewResult;
 
             Assert.NotNull(result);
@@ -128,24 +268,104 @@ namespace HNTAS.Web.UI.Tests.Controllers
         }
 
         [Fact]
-        public void Get_StartPage_ReturnsView()
+        public void StartPage_ReturnsViewResult()
         {
-            var result = _controller.StartPage() as ViewResult;
+            // Arrange
+            var urlHelperMock = SetUpBackLink("Contributor", "YouHaveBeenInvited");
+            _controller.Url = urlHelperMock.Object; // Assign mock to controller.Url
 
-            Assert.NotNull(result);
-            Assert.IsType<ViewResult>(result);
+            // Act
+            var result = _controller.StartPage();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
         }
 
         [Fact]
-        public void Get_Dashboard_ReturnsView()
+        public async Task UserLogin_ValidClaims_UserExistsWithOrgId_RedirectsToDashboard()
         {
+            // Arrange
+            var email = "test@example.com";
+            var oneLoginId = "12345";
+                       
+            _userServiceMock.Setup(s => s.GetUserByOneLoginId(oneLoginId))
+                           .ReturnsAsync(new UserResponse { Id = "user123", OrgId = "org456" });
+                        
+            var claims = new List<Claim>
+            {
+                new Claim("email", email),
+                new Claim("sub", oneLoginId)
+            };
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(claims))
+            };
+
             // Act
-            var result = _controller.Dashboard() as ViewResult;
+            var result = await _controller.UserLogin();
 
             // Assert
-            Assert.NotNull(result);
-            Assert.IsType<ViewResult>(result);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Dashboard", redirectResult.ActionName);
+            Assert.Equal("Contributor", redirectResult.ControllerName);
+            _sessionHelperMock.Verify(s => s.SaveToSession(It.IsAny<HttpContext>(), SessionKeys.UserModel_Id_SessionKey, "user123"), Times.Once);
         }
+
+        [Fact]
+        public async Task UserLogin_MissingClaims_ReturnsStartPageWithErrorMessage()
+        {
+            // Arrange            
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity()) // No claims
+            };
+            var tempData = new TempDataDictionary(_controller.HttpContext, Mock.Of<ITempDataProvider>());
+            _controller.TempData = tempData;
+
+            // Act
+            var result = await _controller.UserLogin();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("StartPage", viewResult.ViewName);
+            Assert.True(_controller.TempData.ContainsKey("ErrorMessage"));
+            Assert.Equal("Unable to retrieve essential user info. Please try again.", _controller.TempData["ErrorMessage"]);
+        }
+        
+        [Fact]
+        public async Task UserLogin_ExistingUserWithoutOrgId_ReturnsView()
+        {
+            // Arrange
+            var email = "test@example.com";
+            var oneLoginId = "12345";
+
+            var claims = new List<Claim>
+            {
+                new Claim("email", email),
+                new Claim("sub", oneLoginId)
+            };
+
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(claims))
+            };
+
+            var existingUser = new UserResponse { Id = "user123", OrgId = null };
+
+            _userServiceMock.Setup(s => s.GetUserByOneLoginId(oneLoginId))
+                            .ReturnsAsync(existingUser);
+
+            // Act
+            var result = await _controller.UserLogin();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            _sessionHelperMock.Verify(s => s.SaveToSession(It.IsAny<HttpContext>(), SessionKeys.UserModel_Id_SessionKey, existingUser.Id), Times.Once);
+        }
+
+        #endregion
 
 
     }
