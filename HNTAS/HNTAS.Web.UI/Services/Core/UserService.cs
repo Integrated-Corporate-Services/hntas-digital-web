0c163e921b6799435ffd1a0cf8959cfe0bbdb996
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-11-06 14:22:48 +0000

    fix for the same contributor login was able to register as a new user to register HN.

--- HNTAS/HNTAS.Web.UI/Services/Core/UserService.cs
+++ HNTAS/HNTAS.Web.UI/Services/Core/UserService.cs
@@ -303,7 +303,7 @@ namespace HNTAS.Web.UI.Services.Core
 
         public async Task<bool?> IsRpUserAsync(string emailId)
         {
-            var users = await _usersApi.IsRpUserAsync(emailId);
+            var users = await _usersApi.ApiUsersIsRpUserEmailIdGetAsync(emailId);
             if (users.IsOk)
             {
                 return users.Ok();
@@ -312,10 +312,32 @@ namespace HNTAS.Web.UI.Services.Core
             {
                 return null;
             }
-            var sanitizedEmailId = emailId?.Replace("\r", "").Replace("\n", "");
-            var errorMessage = $"Unable to determine Regulatory Contact status for user '{sanitizedEmailId}'. API call failed with status code: {users.StatusCode}.";
+            var errorMessage = $"Unable to determine Regulatory Contact status for user '{SanitizeForLogging(emailId)}'. API call failed with status code: {users.StatusCode}.";
             _logger.LogError(errorMessage);
             throw new Exception(errorMessage);
+        }
+
+
+        public async Task<bool?> IsActiveUserAsync(string emailId)
+        {
+            var users = await _usersApi.ApiUsersIsActiveUserEmailIdGetAsync(emailId);
+            if (users.IsOk)
+            {
+                return users.Ok();
+            }
+            else if (users.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            var errorMessage = $"Unable to determine Regulatory Contact status for user '{SanitizeForLogging(emailId)}'. API call failed with status code: {users.StatusCode}.";
+            _logger.LogError(errorMessage);
+            throw new Exception(errorMessage);
+        }
+
+
+        private string SanitizeForLogging(string input)
+        {
+            return input?.Replace("\r", "").Replace("\n", "") ?? string.Empty;
         }
 
     }
