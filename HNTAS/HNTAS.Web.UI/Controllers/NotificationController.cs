7d06b3ecd6ee677f727b4e0fe1c550e0ada526e7
SManokaran90 <sivasankar.manokaran@energysecurity.gov.uk>
2026-01-07 15:26:55 +0000

    Bug/dhb 400 and 399 fix web (#235)
    * fix for bug 400 and 399
    
    * Adde dhb-403-404 changes

---
+++ HNTAS/HNTAS.Web.UI/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
