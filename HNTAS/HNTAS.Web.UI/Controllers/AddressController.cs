17d7c15e6cdd7aa5c2a84f09f73e27f9b7944fa2
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-01-15 00:17:05 +0000

    Feature/dhb 409 address by lat long (#239)
    * Added all features for dhb-409
    
    * Address feature extracted and implemented in AddressController
    
    * Added the EnergyHeatNetworkPrimaryLosses default value to carbon calculator view
    
    * Removed mentions of outdated actions in OrganisationControllerTests

---
+++ HNTAS/HNTAS.Web.UI/Controllers/AddressController.cs
@@ -0,0 +1,108 @@
+﻿using HNTAS.Web.UI.Helpers;
+using HNTAS.Web.UI.Models;
+using HNTAS.Web.UI.Models.Address;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly ILogger<AddressController> _logger;
+        private readonly ISessionHelper _sessionHelper;
+        public AddressController(ILogger<AddressController> logger, ISessionHelper sessionHelper)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _sessionHelper = sessionHelper ?? throw new ArgumentNullException(nameof(sessionHelper));
+        }
+
+        [HttpGet]
+        public IActionResult SearchByPostcodeResults()
+        {
+            string previousStep = _sessionHelper.GetFromSession<string>(HttpContext, SessionKeys.PreviousStepKey);
+            if (previousStep == "HeatNetwork")
+            {
+                this.ShowBackButton("DoesHNHaveAPostcode", "HeatNetwork");
+            }else
+            {
+                this.ShowBackButton("OrganisationAddressByPostcode", "Organisation");
+            }
+
+            SearchAddressByPostcodeModel model = _sessionHelper.GetFromSession<SearchAddressByPostcodeModel>(HttpContext, SessionKeys.SearchAddressByPostcodeModelSessionKey);
+
+            if (model == null)
+            {
+                _logger.LogError("SearchAddressByPostcodeModel is null.");
+                if (previousStep == "heatnetwork")
+                {
+                    return View("DoesHNHaveAPostcode", "HeatNetwork");
+                }
+                else
+                {
+                    return View("OrganisationAddressByPostcode", "Organisation");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult SelectAddress(string selectedAddress)
+        {
+            var addressmodel = _sessionHelper.GetFromSession<SearchAddressByPostcodeModel>(HttpContext, SessionKeys.SearchAddressByPostcodeModelSessionKey);
+            if (addressmodel == null)
+            {
+                _logger.LogError("SearchAddressByPostcodeModel not found in session.");
+                return BadRequest("Session expired or invalid. Please try again.");
+            }
+            addressmodel.SelectedFullAddress = Utility.CapitalizeCommaSeparated(selectedAddress);
+            var addressParts = addressmodel.SelectedFullAddress.Split(",");
+
+            if (addressParts.Length < 3)
+            {
+                _logger.LogError("Malformed address received: {Address}", selectedAddress);
+                return BadRequest("Selected address is not in the expected format. It must contain at least street, town/city, and postcode.");
+            }
+
+            var model = new AddressByStreetOrTownModel
+            {
+                StreetAddress = string.Join(",", addressParts.Take(addressParts.Length - 2)) ?? string.Empty,
+                TownOrCity = addressParts[addressParts.Length - 2] ?? string.Empty,
+                Postalcode = (addressParts[addressParts.Length - 1]).ToUpper() ?? string.Empty,
+                Country = "United Kingdom" ?? string.Empty,
+                Fulladdress = addressmodel.SelectedFullAddress
+            };            
+            // Save the new model to session
+            _sessionHelper.SaveToSession<AddressByStreetOrTownModel>(HttpContext, SessionKeys.AddressByStreetOrTownModelSessionKey, model);
+            string previousStep = _sessionHelper.GetFromSession<string>(HttpContext, SessionKeys.PreviousStepKey);
+            if (previousStep == "HeatNetwork")
+            {
+                return RedirectToAction("ConfirmAddress", "Address");
+            }
+            else
+            {                
+                var organisationModel = _sessionHelper.GetFromSession<OrganisationModel>(HttpContext, SessionKeys.OrganisationCreation_SessionKey);
+
+                if (model == null || organisationModel?.CompanyDetails == null)
+                {
+                    _logger.LogWarning("Missing session data : Required session data is missing or invalid. Address model and Organisation model with CompanyDetails must be present.");
+                    return BadRequest("Missing session data");
+                }
+                organisationModel.CompanyDetails.RegisteredOfficeAddress = model;
+                _sessionHelper.SaveToSession(HttpContext, SessionKeys.OrganisationCreation_SessionKey, organisationModel);
+                return RedirectToAction("CompanyConfirm", "Organisation");
+            }            
+        }
+
+        [HttpGet]
+        public IActionResult ConfirmAddress()
+        {
+            var model = _sessionHelper.GetFromSession<AddressByStreetOrTownModel>(HttpContext, SessionKeys.AddressByStreetOrTownModelSessionKey);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmAddress(AddressByStreetOrTownModel model)
+        {            
+            // Save the confirmed address to session            
+            return RedirectToAction("SaveHNAddressByPostcode", "HeatNetwork");
+        }
+    }
+}
