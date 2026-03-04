10107c1ba2e4453253dc9a4d4ed0c95f18096c53
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-03-04 16:48:43 +0000

    Reapplied missing fix

--- HNTAS/HNTAS.Web.UI/Controllers/OrganisationController.cs
+++ HNTAS/HNTAS.Web.UI/Controllers/OrganisationController.cs
@@ -393,7 +393,6 @@ namespace HNTAS.Web.UI.Controllers
             var orgModel = _sessionHelper.GetFromSession<OrganisationModel>(HttpContext, SessionKeys.OrganisationCreation_SessionKey);
             var userModel = _sessionHelper.GetFromSession<UserModel>(HttpContext, SessionKeys.UserCreation_SessionKey);
             this.ShowBackButton("DeedPoll");
-            ViewBag.NextActionController = "Organisation";
             return View(userModel.ContactDetails);
         }
 
