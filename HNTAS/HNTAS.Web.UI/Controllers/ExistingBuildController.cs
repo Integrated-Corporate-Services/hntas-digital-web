2d539c3fd392661bb058ddd453efc3e40c8df68f
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-03-06 16:39:48 +0000

    Added pages for ExistingBuild, restructured views into separate folders for different builds (#297)

---
+++ HNTAS/HNTAS.Web.UI/Controllers/ExistingBuildController.cs
@@ -0,0 +1,39 @@
+﻿using HNTAS.Web.UI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    public class ExistingBuildController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ShowBackButton("DocumentLibrary", "Home");
+            return View();
+        }
+        public IActionResult Overview()
+        {
+            this.ShowBackButton("Index");
+            return View();
+        }
+        public IActionResult Milestone2()
+        {
+            this.ShowBackButton("Index");
+            return View();
+        }
+        public IActionResult Milestone3A()
+        {
+            this.ShowBackButton("Index");
+            return View();
+        }
+        public IActionResult Milestone3B()
+        {
+            this.ShowBackButton("Index");
+            return View();
+        }
+        public IActionResult Milestone4()
+        {
+            this.ShowBackButton("Index");
+            return View();
+        }
+    }
+}
