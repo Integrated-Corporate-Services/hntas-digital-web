304fcdd88ffdfff15285271dd9f449975828e513
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-04-21 09:43:09 +0100

    Update api client

--- HNTAS/HNTAS.Api.Client/src/HNTAS.Api.Client/Client/TokenProvider`1.cs
+++ HNTAS/HNTAS.Api.Client/src/HNTAS.Api.Client/Client/TokenProvider`1.cs
@@ -27,12 +27,7 @@ namespace HNTAS.Api.Client
         /// </summary>
         protected TTokenBase[] _tokens;
 
-        /// <summary>
-        /// Gets an authentication token to be used in request authorization.
-        /// </summary>
-        /// <param name="header"></param>
-        /// <param name="cancellation"></param>
-        protected internal abstract System.Threading.Tasks.ValueTask<TTokenBase> GetAsync(string header = "", System.Threading.CancellationToken cancellation = default);
+        internal abstract System.Threading.Tasks.ValueTask<TTokenBase> GetAsync(string header = "", System.Threading.CancellationToken cancellation = default);
 
         /// <summary>
         /// Instantiates a TokenProvider.
@@ -41,6 +36,9 @@ namespace HNTAS.Api.Client
         public TokenProvider(IEnumerable<TTokenBase> tokens)
         {
             _tokens = tokens.ToArray();
+
+            if (_tokens.Length == 0)
+                throw new ArgumentException("You did not provide any tokens.");
         }
     }
 }
