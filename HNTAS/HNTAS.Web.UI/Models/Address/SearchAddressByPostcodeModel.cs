7658c3953f8fbfafdb8ee136a5217dd61888f7ca
Kuldeep-es <Kuldeep.Saini@energysecurity.gov.uk>
2026-06-05 16:49:25 +0100

    validation fix

--- HNTAS/HNTAS.Web.UI/Models/Address/SearchAddressByPostcodeModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/Address/SearchAddressByPostcodeModel.cs
@@ -5,7 +5,7 @@ namespace HNTAS.Web.UI.Models.Address
     public class SearchAddressByPostcodeModel
     {
         [Required(ErrorMessage = "Please enter the postcode.")]
-        [RegularExpression(@"^(GIR 0AA|[A-PR-UWYZ](?:[0-9]{1,2}|[A-HK-Y][0-9]{1,2}|[0-9][A-HJKS-UW]|[A-HK-Y][0-9][ABEHMNPRV-Y]) ?[0-9][ABD-HJLNP-UW-Z]{2})$",
+        [RegularExpression(@"^[A-Za-z]{1,2}\d[A-Za-z\d]?\s*\d[A-Za-z]{2}$",
         ErrorMessage = "Please enter a valid UK postcode.")]
         public string? Postcode { get; set; }
         public string[]? Addresses { get; set; }
