51bedb34d35503293d03dc11e675de519a5afcee
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-06-09 11:00:07 +0100

    Cookie banner added

---
+++ HNTAS/HNTAS.Web.UI/Controllers/CookiesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    [Route("cookies")]
+    public class CookiesController : Controller
+    {
+        [HttpPost("/cookies/accept")]
+        public IActionResult Accept()
+        {
+            Response.Cookies.Append("cookie_consent", "accepted", new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddMilliseconds(500),
+                IsEssential = true
+            });
+
+            TempData["cookie_banner_action"] = "accepted";
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+        [HttpPost("/cookies/reject")]
+        public IActionResult Reject()
+        {
+            Response.Cookies.Append("cookie_consent", "rejected", new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddMilliseconds(500),
+                IsEssential = true
+            });
+
+            TempData["cookie_banner_action"] = "rejected";
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+    }
+}
