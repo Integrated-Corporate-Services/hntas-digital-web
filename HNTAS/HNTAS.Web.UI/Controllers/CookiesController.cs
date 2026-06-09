09a8f75d0b5b9c0aa232ebf3742e205fdf0f57ba
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-06-09 12:53:50 +0100

    added function for safe url

--- HNTAS/HNTAS.Web.UI/Controllers/CookiesController.cs
+++ HNTAS/HNTAS.Web.UI/Controllers/CookiesController.cs
@@ -12,12 +12,13 @@ namespace HNTAS.Web.UI.Controllers
             Response.Cookies.Append("cookie_consent", "accepted", new CookieOptions
             {
                 Expires = DateTimeOffset.UtcNow.AddYears(1),
-                IsEssential = true
+                IsEssential = true,
+                Secure = true
             });
 
             TempData["cookie_banner_action"] = "accepted";
 
-            return Redirect(Request.Headers["Referer"].ToString());
+            return LocalRedirect(GetSafeReturnUrl());
         }
 
         [HttpPost("/cookies/reject")]
@@ -27,12 +28,40 @@ namespace HNTAS.Web.UI.Controllers
             Response.Cookies.Append("cookie_consent", "rejected", new CookieOptions
             {
                 Expires = DateTimeOffset.UtcNow.AddYears(1),
-                IsEssential = true
+                IsEssential = true,
+                Secure = true
             });
 
             TempData["cookie_banner_action"] = "rejected";
 
-            return Redirect(Request.Headers["Referer"].ToString());
+            return LocalRedirect(GetSafeReturnUrl());
+        }
+
+        private string GetSafeReturnUrl()
+        {
+            const string fallbackUrl = "/";
+            var referer = Request.Headers["Referer"].ToString();
+            if (string.IsNullOrWhiteSpace(referer))
+            {
+                return fallbackUrl;
+            }
+            if (!Uri.TryCreate(referer, UriKind.RelativeOrAbsolute, out var parsedUri))
+            {
+                return fallbackUrl;
+            }
+            if (parsedUri.IsAbsoluteUri)
+            {
+                var sameHost = string.Equals(parsedUri.Host, Request.Host.Host, StringComparison.OrdinalIgnoreCase);
+                var sameScheme = string.Equals(parsedUri.Scheme, Request.Scheme, StringComparison.OrdinalIgnoreCase);
+                if (!sameHost || !sameScheme)
+                {
+                    return fallbackUrl;
+                }
+                var localPath = parsedUri.PathAndQuery + parsedUri.Fragment;
+                return Url.IsLocalUrl(localPath) ? localPath : fallbackUrl;
+            }
+            var relativeUrl = parsedUri.ToString();
+            return Url.IsLocalUrl(relativeUrl) ? relativeUrl : fallbackUrl;
         }
     }
 }
