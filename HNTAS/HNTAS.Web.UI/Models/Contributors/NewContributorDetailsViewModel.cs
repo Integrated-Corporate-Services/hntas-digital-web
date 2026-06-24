86c13549e9b3c2de168c96a18bfb1282a3ce635d
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-06-24 15:44:14 +0100

    Changes updated as per dhb-690 (#525)
    * Changes updated as per dhb-690
    
    * Changes updated as per dhb-690

--- HNTAS/HNTAS.Web.UI/Models/Contributors/NewContributorDetailsViewModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/Contributors/NewContributorDetailsViewModel.cs
@@ -5,9 +5,13 @@ namespace HNTAS.Web.UI.Models.Contributors
     public class NewContributorDetailsViewModel
     {
         [Required(ErrorMessage="Enter their first name")]
+        [RegularExpression(@"^[a-zA-Z\-]+$", ErrorMessage = "Enter a valid first name using letters, numbers or common punctuation only.")]
+        [MaxLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage="Enter their last name")]
+        [RegularExpression(@"^[a-zA-Z\-]+$", ErrorMessage = "Enter a valid last name using letters, numbers or common punctuation only.")]
+        [MaxLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage="Enter their email address")]
