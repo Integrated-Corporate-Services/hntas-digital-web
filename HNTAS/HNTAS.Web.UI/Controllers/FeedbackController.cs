52b6dfa0b55ee588de6cefab8f9eec587be7cbab
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-07-01 21:46:42 +0100

    Added ui components for feedback form

--- HNTAS/HNTAS.Web.UI/Controllers/FeedbackController.cs
+++ HNTAS/HNTAS.Web.UI/Controllers/FeedbackController.cs
@@ -1,14 +1,30 @@
 ﻿using HNTAS.Web.UI.Helpers;
+using HNTAS.Web.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HNTAS.Web.UI.Controllers
 {
     public class FeedbackController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             this.ShowBackButton("StartPage", "Home");
-            return View();
+            return View(new FeedbackFormModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(FeedbackFormModel model)
+        {
+            this.ShowBackButton("StartPage", "Home");
+            if (!ModelState.IsValid)
+            {                
+                return View(model);
+            }
+            // Process the feedback (e.g., save to database, send email, etc.)
+            // For now, we will just redirect to a thank you page.
+            return View(new FeedbackFormModel());
         }
     }
 }
