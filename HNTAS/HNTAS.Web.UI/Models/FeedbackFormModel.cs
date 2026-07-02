f7f081c7b68b11ea454088b5204a4f4264f0d41f
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-07-02 16:10:54 +0100

    Added Feedbackform

---
+++ HNTAS/HNTAS.Web.UI/Models/FeedbackFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HNTAS.Web.UI.Models
+{
+    public class FeedbackFormModel
+    {
+        [Required(ErrorMessage = "Please select an option.")]
+        public string SatisfactionLevel { get; set; }
+        public string? Feedback { get; set; }
+    }
+}
