59cdb9af550857ecf955f35ef3ab49c9d23ae652
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-07-09 11:19:24 +0100

    removed unused files

--- HNTAS/HNTAS.Web.UI/Services/IS3UploadService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HNTAS.Web.UI.Services
-{
-    public interface IS3UploadService
-    {
-        Task<string> UploadFileAsync(IFormFile file, string keyPrefix);
-        Task<Stream?> GetFileAsync(string key);
-    }
-}
