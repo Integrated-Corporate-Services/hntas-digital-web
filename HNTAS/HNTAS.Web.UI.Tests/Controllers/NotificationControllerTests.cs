bc34c08a7b81001675b78f43d3d59a9b5c7aad12
Kuldeep-es <Kuldeep.Saini@energysecurity.gov.uk>
2026-07-13 14:46:04 +0100

    unit test code coverage

--- HNTAS/HNTAS.Web.UI.Tests/Controllers/NotificationControllerTests.cs
+++ HNTAS/HNTAS.Web.UI.Tests/Controllers/NotificationControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using HNTAS.Web.UI.Controllers;
+﻿using HNTAS.Api.Client.Model;
+using HNTAS.Web.UI.Controllers;
 using HNTAS.Web.UI.Services.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,71 @@ namespace HNTAS.Web.UI.Tests.Controllers
             var tempData = new TempDataDictionary(controller.ControllerContext.HttpContext, Mock.Of<ITempDataProvider>());
             controller.TempData = tempData;
             return controller;
-        }        
+        }
+
+        [Fact]
+        public async Task Index_ReturnsViewResult_WithNotificationHistory()
+        {
+            // Arrange
+            var userId = "testUser";
+            var notificationHistoryResponse = new NotificationHistoryResponse
+            {
+                Items = new List<NotificationHistoryData>
+                {
+                    new NotificationHistoryData { Id = "1", Timestamp = DateTime.UtcNow },
+                    
+                },
+            };
+            _mockNotificationHistoryService.Setup(service => service.GetNotificationHistory(It.IsAny<NotificationHistoryRequest>()))
+                .ReturnsAsync(notificationHistoryResponse);
+            // Act
+            var result = await _controller.Index(userId);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<NotificationHistoryResponse>(viewResult.Model);
+            Assert.Equal(1, model.Items.Count);
+        }
+
+        [Fact]
+        public async Task Index_Exception_ReturnsViewResult_EmpbyNotificationHistory()
+        {
+            // Arrange
+            var userId = "testUser";
+            var notificationHistoryResponse = new NotificationHistoryResponse
+            {
+                Items = new List<NotificationHistoryData>
+                {
+                    new NotificationHistoryData { Id = "1", Timestamp = DateTime.UtcNow },
+
+                },
+            };
+            _mockNotificationHistoryService.Setup(service => service.GetNotificationHistory(It.IsAny<NotificationHistoryRequest>()))
+                .Throws(new Exception("Test exception"));
+            // Act
+            var result = await _controller.Index(userId);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<NotificationHistoryResponse>(viewResult.Model);
+            Assert.Equal(0, model.Items.Count);
+        }
+
+        [Theory]
+        [InlineData("Heat network details", "AddNetworkDetails")]
+        [InlineData("DDH and contributors", "ManageContributors")]
+        [InlineData("Network managers", "ManageLeads")]        
+        public async Task ExecuteAction_ReturnsRedirectResult_WhenActionDetailsAreValid(string action, string targetAction)
+        {
+            // Arrange
+            var actionDetails = new Dictionary<string, string>
+            {
+                { "hnid", "testHnid" },
+                { "action", action }
+            };
+            // Act
+            var result = await _controller.ExecuteAction(actionDetails);
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal(targetAction, redirectResult.ActionName);
+        }
     }
 }
