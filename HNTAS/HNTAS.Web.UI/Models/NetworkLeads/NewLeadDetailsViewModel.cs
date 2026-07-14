c342e8c5c735dd3b1ac690b17a41997c89f85734
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2026-07-14 15:34:29 +0100

    Feature/toqa dhb 706 (#575) (#576)
    * updated the casing of acronym in link
    
    * Updated hn manager name to accept (-) and the NewContributorDetails page showing correct user to add.

--- HNTAS/HNTAS.Web.UI/Models/NetworkLeads/NewLeadDetailsViewModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/NetworkLeads/NewLeadDetailsViewModel.cs
@@ -5,12 +5,12 @@ namespace HNTAS.Web.UI.Models.NetworkLeads
 {
     public class NewLeadDetailsViewModel
     {
-        [Required(ErrorMessage = "First name is required.")]
-        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "First name can only contain letters and spaces.")]
+        [Required(ErrorMessage = "Enter your first name.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\.\,\:\'\&\-]+$", ErrorMessage = "Enter a valid first name using letters, numbers or common punctuation only.")]
         [MaxLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Last name is required.")]
-        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Last name can only contain letters and spaces.")]
+        [Required(ErrorMessage = "Enter your last name.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\.\,\:\'\&\-]+$", ErrorMessage = "Enter a valid last name using letters, numbers or common punctuation only.")]
         [MaxLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
         public string LastName { get; set; }
         [Required(ErrorMessage = "Email is required.")]
