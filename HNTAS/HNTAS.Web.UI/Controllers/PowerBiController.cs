e7c2ced96dc18f7f87d13b8126b6eebd50ad90cb
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2026-07-20 15:09:27 +0100

    added powerbi links in the dashboard

---
+++ HNTAS/HNTAS.Web.UI/Controllers/PowerBiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNTAS.Web.UI.Controllers
+{
+    public class PowerBiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
